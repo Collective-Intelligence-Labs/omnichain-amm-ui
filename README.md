1a039e92105c4a800ecb2d522a4f02a335694459
Alex Shkor <alex.shkor@gmail.com>
2023-09-10 17:50:25 +0200

    inital union structure implemented

--- README.md
+++ README.md
@@ -22,3 +22,24 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+## Instal Brew
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+touch ~/.zshrc
+export PATH=/opt/homebrew/bin:$PATH
+source ~/.zshrc
+
+## Install Protobuff
+
+brew install protobuf
+
+
+## Create scheme files for proto
+
+go into Proto files folder
+
+protoc --js_out=import_style=commonjs,binary:./ event.proto
+
+protoc --js_out=import_style=commonjs,binary:./ command.proto
--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
 <template>
   <div id="app">
     <b-navbar toggleable="lg">
-      <b-navbar-brand href="#">Omnichain AMM DEX {{account}}</b-navbar-brand>
+      <b-navbar-brand href="#">Mission Union {{account}}</b-navbar-brand>
  
         <b-navbar-nav>
-          <b-nav-item href="/swap" class="nav-item-fancy">Swap</b-nav-item>
-          <b-nav-item href="/add-liquidity" class="nav-item-fancy">Add Liquidity</b-nav-item>
-          <b-nav-item href="/remove-liquidity" class="nav-item-fancy">Remove Liquidity</b-nav-item>
+          <b-nav-item href="/swap" class="nav-item-fancy">My Mission Unions</b-nav-item>
+          <b-nav-item href="/add-liquidity" class="nav-item-fancy">Join Mission Union</b-nav-item>
+          <b-nav-item href="/remove-liquidity" class="nav-item-fancy">Create a Mission Union</b-nav-item>
           <b-nav-item href="/deposit-withdraw" class="nav-item-fancy">Deposit/Withdraw Funds</b-nav-item>
     
         </b-navbar-nav>
